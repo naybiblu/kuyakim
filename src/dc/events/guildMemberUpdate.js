f7092540fb045e04cc0bd53ad4c807fc170152c7
iamsuperv15@gmail.com <iamsuperv15@gmail.com>
2024-01-06 22:52:09 +0800

    Ignoring node modules

--- src/dc/events/guildMemberUpdate.js
+++ src/dc/events/guildMemberUpdate.js
@@ -6,7 +6,7 @@ module.exports = {
   once: false,
   async run(oldM, newM) {
 
-    if (oldM.roles.cache.has("910315515237503066") && !newM.roles.cache.has("910315515237503066")) newM.guild.channels.resolve("880069748740735029").send({
+    if (newM.roles.cache.has("880121950683406407")) newM.guild.channels.resolve("880069748740735029").send({
     content: `\'Andito na si <@${newM.id}>!`,
     files: [ await dynamicGraphics.welcome(newM) ]
   });
