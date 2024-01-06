9d21e5c34bef9383f54e8cd20c7344d6083ad208
iamsuperv15@gmail.com <iamsuperv15@gmail.com>
2024-01-06 23:39:04 +0800

    Fixed double instances of welcome banner

--- src/dc/events/guildMemberUpdate.js
+++ src/dc/events/guildMemberUpdate.js
@@ -6,7 +6,7 @@ module.exports = {
   once: false,
   async run(oldM, newM) {
 
-    if (newM.roles.cache.has("880121950683406407")) newM.guild.channels.resolve("880069748740735029").send({
+    if (!newM.roles.cache.has("910315515237503066") && newM.roles.cache.has("880121950683406407")) newM.guild.channels.resolve("880069748740735029").send({
     content: `Nandito na si <@${newM.id}>!`,
     files: [ await dynamicGraphics.welcome(newM) ]
   });
