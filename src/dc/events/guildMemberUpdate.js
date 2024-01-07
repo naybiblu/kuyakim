24cad454abca17feb7929a02a0922ade6117ed1e
iamsuperv15@gmail.com <iamsuperv15@gmail.com>
2024-01-07 13:20:00 +0800

    Fixed welcome banner logic

--- src/dc/events/guildMemberUpdate.js
+++ src/dc/events/guildMemberUpdate.js
@@ -6,7 +6,7 @@ module.exports = {
   once: false,
   async run(oldM, newM) {
 
-    if (!newM.roles.cache.has("910315515237503066") && newM.roles.cache.has("880121950683406407") && newM.roles.cache.size <= 1) newM.guild.channels.resolve("880069748740735029").send({
+    if (oldM.roles.cache.has("910315515237503066") && newM.roles.cache.has("880121950683406407") && !newM.roles.cache.has("910315515237503066")) newM.guild.channels.resolve("880069748740735029").send({
     content: `Nandito na si <@${newM.id}>!`,
     files: [ await dynamicGraphics.welcome(newM) ]
   });
